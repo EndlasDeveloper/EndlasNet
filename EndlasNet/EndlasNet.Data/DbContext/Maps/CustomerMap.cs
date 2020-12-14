9a9ad99277e32c79a4f44460f3728f894c8526ee
Nate <nathan.saunders@endlas.com>
2020-12-14 13:03:28 -0700

    Got project setup to successfully add a migration and update the database based on the migration data. Only entity is customer. Builds the db to the local visual studio db extension (SQL Server Object Explorer).

---
+++ EndlasNet/EndlasNet.Data/DbContext/Maps/CustomerMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndlasNet.Data
+{
+    public class CustomerMap
+    {
+        public CustomerMap(EntityTypeBuilder<Customer> entityBuilder)
+        {
+            // make .NET happpy
+            Contract.Requires(entityBuilder != null);
+            // set PK
+            entityBuilder.HasKey(c => c.CustomerId);  
+            // not null
+            entityBuilder.Property(c => c.CompanyName).IsRequired();
+            entityBuilder.Property(c => c.PointOfContact).IsRequired();
+            entityBuilder.Property(c => c.Address).IsRequired();
+            entityBuilder.Property(c => c.PhoneNumber).IsRequired();
+        }
+    }
+}
---
+++ EndlasNet/EndlasNet.Data/Migrations/20201214200041_testMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EndlasNet.Data.Migrations
+{
+    public partial class testMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PointOfContact = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
