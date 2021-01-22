13453dd8ff7a88499e36ac37e55b10801cebc23a
Nate <nathan.saunders@endlas.com>
2021-01-22 12:29:50 -0700

    Added unit test project. Added unit test for the shaHash method for hashing authorization strings. All tests pass.

---
+++ EndlasNet/EndlasNet.UnitTest/SecurityTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using EndlasNet.Data;
+namespace EndlasNet.UnitTest
+{
+    public class ShaHashTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ComputeSha256HashTest()
+        {
+            // ARRANGE
+            var testAuthStr = UnitTestUtil.getRandomString(8);
+            var differentAuthStr = UnitTestUtil.getRandomString(8);
+
+            // ACT
+            var hashedAuthStr = ShaHash.ComputeSha256Hash(testAuthStr);
+            var hashedDiffAuthStr = ShaHash.ComputeSha256Hash(differentAuthStr);
+            var hashedAuthStr2 = ShaHash.ComputeSha256Hash(testAuthStr);
+
+            // ASSERT
+            // make sure the same auth str gives same hash
+            Assert.AreEqual(hashedAuthStr, hashedAuthStr2);
+            // make sure different auth strs give different hash
+            Assert.AreNotEqual(hashedAuthStr, hashedDiffAuthStr);
+        }
+    }
+}
---
+++ EndlasNet/EndlasNet.UnitTest/UnitTestUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EndlasNet.UnitTest
+{
+    public static class UnitTestUtil
+    {
+        static Random rd = new Random();
+        public static string getRandomString(int stringLength)
+        {
+
+            const string allowedChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz0123456789!@$?_-";
+            char[] chars = new char[stringLength];
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                chars[i] = allowedChars[rd.Next(0, allowedChars.Length)];
+            }
+
+            return new string(chars);
+        }
+    }
+}
