62ee8fde2db6396f7de438a83bd1a5f62ab1a41a
EndlasDeveloper <61885923+EndlasDeveloper@users.noreply.github.com>
2021-04-04 07:44:59 -0600

    Added interfaces and classes for Work/Job/WorkOrder. All methods exist, but are not implemented.

---
+++ EndlasNet/EndlasNet.Data/Repositories/Work/Works/WorkOrderRepo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndlasNet.Data
+{
+    public class WorkOrderRepo : IWorkRepo
+    {
+        public Task AddRow(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteRow(Guid? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<object>> GetAllRows()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetRow(Guid? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetRowNoTracking(Guid? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRow(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RowExists(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRow(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
