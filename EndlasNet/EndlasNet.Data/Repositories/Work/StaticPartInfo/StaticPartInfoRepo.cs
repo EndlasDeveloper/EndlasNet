ab8ac99c3484ca0e9a652b6268eb8a66873c23c3
EndlasDeveloper <61885923+EndlasDeveloper@users.noreply.github.com>
2021-04-04 07:21:01 -0600

    Added repository class and interface for StaticPartInfo. Moved some of the file structure around.

---
+++ EndlasNet/EndlasNet.Data/Repositories/Work/StaticPartInfo/StaticPartInfoRepo.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndlasNet.Data
+{
+    public class StaticPartInfoRepo : IStaticPartInfoRepo
+    {
+        private readonly EndlasNetDbContext _db;
+
+        public StaticPartInfoRepo(EndlasNetDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddRow(object obj)
+        {
+            try
+            {
+                await _db.StaticPartInfo.AddAsync((StaticPartInfo)obj);
+                await _db.SaveChangesAsync();
+            }
+            catch (InvalidCastException) { }
+        }
+
+        public async Task DeleteRow(Guid? id)
+        {
+            var row = await _db.StaticPartInfo
+                .FirstOrDefaultAsync(s => s.StaticPartInfoId == id);
+            _db.Remove(row);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<object>> GetAllRows()
+        {
+            return await _db.StaticPartInfo.ToListAsync();
+        }
+
+        public async Task<object> GetRow(Guid? id)
+        {
+            return await _db.StaticPartInfo
+                .FirstOrDefaultAsync(s => s.StaticPartInfoId == id);
+        }
+
+        public async Task<object> GetRowNoTracking(Guid? id)
+        {
+            return await _db.StaticPartInfo
+                .AsNoTracking()
+                .FirstOrDefaultAsync(s => s.StaticPartInfoId == id);
+        }
+
+        public Task RemoveRow(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RowExists(Guid id)
+        {
+            return await _db.StaticPartInfo
+                .AnyAsync(s => s.StaticPartInfoId == id);
+        }
+
+        public async Task UpdateRow(object obj)
+        {
+            try
+            {
+                _db.Update((StaticPartInfo)obj);
+                await _db.SaveChangesAsync();
+            }
+            catch (InvalidCastException) { }
+        }
+    }
+}
