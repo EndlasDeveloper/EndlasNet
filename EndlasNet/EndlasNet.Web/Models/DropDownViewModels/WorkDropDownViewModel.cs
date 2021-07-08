453ae0bf33a78b8551dd2e5a0460dab3e0272214
EndlasDeveloper <61885923+EndlasDeveloper@users.noreply.github.com>
2021-07-08 12:37:26 -0600

    Added view model class for work dropdowns. Creates display string using work endlas number - work description.

---
+++ EndlasNet/EndlasNet.Web/Models/DropDownViewModels/WorkDropDownViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EndlasNet.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EndlasNet.Web.Models.DropDownViewModels
+{
+    public class WorkDropDownViewModel
+    {
+        public WorkDropDownViewModel(Work work)
+        {
+            WorkId = work.WorkId;
+            DropDownWorkItemDisplayStr = work.EndlasNumber + " - " + work.WorkDescription;
+        }
+        [Display(Name = "Work")]
+        public Guid WorkId { get; set; }
+        public string DropDownWorkItemDisplayStr { get; set; }
+    }
+}
